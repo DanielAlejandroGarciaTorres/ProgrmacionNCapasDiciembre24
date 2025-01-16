ba6d0e3281220eb05652bf170bfda5ba0ac757f2
digis <digis@DESKTOP-Q0UH1ID>
2025-01-16 12:55:29 -0600

    Carga masiva txt

--- src/main/java/com/digis01/DGarciaProgramacionNCapasDiciembre24/Controller/AlumnoController.java
+++ src/main/java/com/digis01/DGarciaProgramacionNCapasDiciembre24/Controller/AlumnoController.java
@@ -10,6 +10,10 @@ import com.digis01.DGarciaProgramacionNCapasDiciembre24.ML.Colonia;
 import com.digis01.DGarciaProgramacionNCapasDiciembre24.ML.Direccion;
 import com.digis01.DGarciaProgramacionNCapasDiciembre24.ML.Result;
 import com.digis01.DGarciaProgramacionNCapasDiciembre24.ML.Semestre;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/Alumno")
@@ -46,7 +51,7 @@ public class AlumnoController {
         Alumno alumnoBusqueda = new Alumno();
         alumnoBusqueda.Semestre = new Semestre();
         model.addAttribute("alumnoBusqueda", alumnoBusqueda);
-        
+
         if (result.correct) {
             model.addAttribute("listaAlumno", result.objects);
         } else {
@@ -54,9 +59,9 @@ public class AlumnoController {
         }
         return "AlumnoIndex";
     }
-    
+
     @PostMapping
-    public String Index(@ModelAttribute Alumno alumnoBusqueda, Model model){
+    public String Index(@ModelAttribute Alumno alumnoBusqueda, Model model) {
         model.addAttribute("alumnoBusqueda", alumnoBusqueda);
         //alumnoDAOImplementation.GetAllDinamico(alumnoBusqueda);
         return "AlumnoIndex";
@@ -105,17 +110,17 @@ public class AlumnoController {
             if (IdAlumno > 0 && IdDireccion > 0) {
                 /*consumir GetbyidDireccion */
                 //alumnoDAOImplementation.DireccionAlumnoByIdDireccion(IdDireccion)
-                
+
                 AlumnoDireccion alumnoDireccion = new AlumnoDireccion();
                 alumnoDireccion.Alumno = new Alumno();
                 alumnoDireccion.Alumno.setIdAlumno(14);
-                
+
                 alumnoDireccion.Direccion = new Direccion();
                 alumnoDireccion.Direccion.setIdDireccion(4);
                 alumnoDireccion.Direccion.setCalle("calle x");
                 alumnoDireccion.Direccion.setNumeroInterior("32A");
                 model.addAttribute("alumnoDireccion", alumnoDireccion);
-                
+
                 // lleno un alumnoDireccion
                 System.out.println(".");
             } else if (IdAlumno > 0 && IdDireccion == 0) {
@@ -125,9 +130,8 @@ public class AlumnoController {
             }
         } else {
             // editar un usaurio
-            
+
         }
-        
 
 //        2.Editar usaurio (direccion nula)
 //        3.Agregar nueva dirección
@@ -154,6 +158,60 @@ public class AlumnoController {
         Result result = municipioDAOImplementation.MunicipioByEstado(IdEstado);
 
         return result;
+    }
+
+    @GetMapping("/CargaMasiva")
+    public String Inicio() {
+        return "CargaMasivaIndex";
+    }
+
+    @PostMapping("/CargaMasiva")
+    public String CargaMasiva(@RequestParam MultipartFile archivo) {
+
+        if (archivo != null && !archivo.isEmpty()) {
+            
+            // CargaMasiva.txt
+            // split -> ["CargaMasiva", "txt"]
+            String fileExension = archivo.getOriginalFilename().split("\\.")[1];
+
+            if (fileExension.equals("txt")) {
+                ProcesarArchivo(archivo);
+            }
+
+        }
+
+        return "";
+    }
+    
+    private boolean ProcesarArchivo(MultipartFile archivo){
+        
+        try{
+            
+            InputStream inputStream = archivo.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            
+            String linea = "";
+            
+            while ( (linea = bufferedReader.readLine()) != null ) {                
+                // nombre|apaterno| ....
+                String[] campos = linea.split("\\|");
+                
+                AlumnoDireccion alumnoDireccion = new AlumnoDireccion();
+                alumnoDireccion.Alumno = new Alumno();
+                alumnoDireccion.Alumno.setNombre(campos[0]);
+                /*...*/
+//                alumnoDireccion.Alumno.setFechaNacimiento(new Date());
+
+
+//                alumnoDAOImplementation.Add(alumnoDireccion);
+            }
+            
+        }catch (Exception ex){
+            return false;
+        }
+        
+        return true;
+        
     }
 
 }
